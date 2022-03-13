76e0c909628eca899f68f063beeb2cd3404f9965
redsemin <semin1125@gmail.com>
2022-03-13 12:36:20 +0900

    Initial commit

---
+++ src/main/java/New.java
@@ -0,0 +1,14 @@
+
+public class New{
+    public static void main(String[] args ){
+        int box;
+        box = 5;
+        int x=3;
+
+        System .out.println("box"+box);
+        System .out.println("x"+x);
+
+        box=10;
+        System.out.println("box"+box);
+    }
+}
